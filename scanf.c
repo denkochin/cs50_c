296cd2377638753f5336eafa70af6f33301161a0
Denis Kochin <denkochinsp@gmail.com>
2021-10-04 18:38:06 +0000

    Using scanf to read a string of 4 bites

--- scanf.c
+++ scanf.c
@@ -2,8 +2,14 @@
 
 int main(void)
 {
-    int x;
-    printf("x: ");
-    scanf("%i", &x);
-    printf("x: %i\n", x);
+    // int x;
+    // printf("x: ");
+    // scanf("%i", &x);
+    // printf("x: %i\n", x);
+
+    char s[4];
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+
 }
