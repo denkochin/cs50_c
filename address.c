5192dca3be5405d5b5ab3d2c1770f9ad1818eced
Denis Kochin <denkochinsp@gmail.com>
2021-10-04 10:53:24 +0000

    Change address.c adding char *, pointer arithmetic and Segmentation fault

--- address.c
+++ address.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
+
 // & - 'address off' operator. What address the var is stored in.
 // * - 'look inside address'. Go into the address.
 // *p - declaring a pointer. Var that storing a pointer.
 
 int main(void)
 {
-    int n = 50;
-    int *p = &n;
-    printf("%i\n", *p);
+    //int n = 50;
+    //int *p = &n;
+    //printf("%i\n", *p);
+
+    // char *s - is the actual implementation of string in C
+    char *s = "HI!";
+    // then s variable is a pointer with the address of a first char of a string
+    // then *s is a "go to the address"
+    printf("%c\n", *s);
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
+    printf("%i\n", *(s+3));
+    printf("%c\n", *(s+100000));
 }
