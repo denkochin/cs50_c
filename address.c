9682cf52bbee71578b24d6a054d4ba23e1316fbf
Denis Kochin <denkochinsp@gmail.com>
2021-10-04 08:30:19 +0000

    Add a var to pointer

--- address.c
+++ address.c
@@ -2,9 +2,11 @@
 
 // & - 'address off' operator. What address the var is stored in.
 // * - 'look inside address'. Go into the address.
+// *p - declaring a pointer. Var that storing a pointer.
 
 int main(void)
 {
     int n = 50;
-    printf("%i\n", *&n);
+    int *p = &n;
+    printf("%p\n", p);
 }
